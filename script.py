9166e2e921c66c3deff5a031fb623df6d9fccd74
Ventthos <ferra04@live.com.mx>
2022-11-10 10:48:26 -0600

    XD
    solo puse lo del mouse

--- script.py
+++ script.py
@@ -3,13 +3,16 @@ import pygame
 pygame.init()
 width = 1000
 height = 500
-window = pygame.display.set_mode((width, height))
+win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Hanal Pixan")
 
 #Posicion mouse
 mx, my = pygame.mouse.get_pos()
 
 # por ahora veremos
+
+def draw_win():
+    win.fill((0, 0, 0))
 class Cuadricula:
     def __init__(self, filas, columnas):
         self.filas = filas
@@ -28,6 +31,8 @@ run = True
 
 while run:
 
+    # checar si mantiene el presionado
+    mb = pygame.mouse.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
