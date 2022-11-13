6c6c73e6813e08b9ba9392b91fbdd120f6b27b63
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-11-13 08:22:45 -0600

    ya jalaaaa

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 width = 1280
@@ -14,10 +15,75 @@ class Piñata:
         self.x = x
         self.y = y
         self.image = ""
-        #radio por ahora pq ajam
+        # radio por ahora pq ajam
         self.radius = 35
+        self.color = (255, 0, 0)
+
     def dibujarDefensa(self):
-        pygame.draw.circle(win, (255, 0, 0), (self.x, self.y), self.radius)
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+    #en cuadrado
+    def dibujarDefensaEC(self, x, y):
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+
+class Piñata2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = ""
+        # radio por ahora pq ajam
+        self.radius = 35
+        self.color = (255, 255, 0)
+    def dibujarDefensa(self):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+    def dibujarDefensaEC(self, x, y):
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+
+class Piñata3:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = ""
+        # radio por ahora pq ajam
+        self.radius = 35
+        self.color = (255, 0, 255)
+    def dibujarDefensa(self):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+    def dibujarDefensaEC(self, x, y):
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+
+class Piñata4:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = ""
+        # radio por ahora pq ajam
+        self.radius = 35
+        self.color = (255, 255, 255)
+    def dibujarDefensa(self):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+    def dibujarDefensaEC(self, x, y):
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+
+class Piñata5:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = ""
+        # radio por ahora pq ajam
+        self.radius = 35
+        self.color = (0, 255, 255)
+    def dibujarDefensa(self):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+    def dibujarDefensaEC(self, x, y):
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
 
 
 class RectanguloPlantas:
@@ -40,13 +106,9 @@ class CuadroPlantas:
         self.x = x
         self.y = y
         self.indice = indice
-        #IMPORTANTE va a contener una piñata por pruebas, pero luego que hagamos para que escoja las plantas, va a
-        #contener [] y luego vamos a recorrer el arreglo de plantas escogidas y ponerle a cada uno lo que contiene
-        self.contains = [Piñata(self.x + 35, self.y + 35),
-                         Piñata(self.x + 35, self.y + 35),
-                         Piñata(self.x + 35, self.y + 35),
-                         Piñata(self.x + 35, self.y + 35),
-                         Piñata(self.x + 35, self.y + 35)]
+        # IMPORTANTE va a contener una piñata por pruebas, pero luego que hagamos para que escoja las plantas, va a
+        # contener [] y luego vamos a recorrer el arreglo de plantas escogidas y ponerle a cada uno lo que contiene
+        self.contains = []
 
     def dibujarCuadrado(self):
         pygame.draw.rect(win, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
@@ -54,13 +116,16 @@ class CuadroPlantas:
     def detectarClick(self, mx, my):
         if (self.y + self.height) > my > self.y:
             if self.x < mx < (self.x + self.width):
-                print(f"Escogio el cuadrado {self.indice}")
-                #si no tiene agarrado nada entonces agarra si no pues no vea
+                print(f"Escogio el cuadrado {self.indice}, que contiene {self.contains}")
+                # si no tiene agarrado nada entonces agarra si no pues no vea
                 if len(ahoritaTiene) == 0:
-                    #Crea un objeto nuevo fuera de la pantalla para agregar
-                    ahoritaTiene.append(Piñata(-100,-100))
-    
+                    # Crea un objeto nuevo fuera de la pantalla para agregar
+                    defensa = self.contains[self.indice]
+                    ahoritaTiene.append(defensa)
+
     def mostrarLoQueContiene(self):
+        self.contains[self.indice].x = self.x + self.width//2
+        self.contains[self.indice].y = self.y + self.height//2
         self.contains[self.indice].dibujarDefensa()
 
 
@@ -82,17 +147,18 @@ class RectanguloOscuro:
     def detectarClick(self, mx, my):
         if (self.y + self.height) > my > self.y:
             if self.x < mx < (self.x + self.width):
-                print(f"hizo click en el cuadrado de la columna {self.columna}, fila {self.fila}. indice: {self.indice}contiene {self.contiene}")
-                #si tienes algo en el mouse y la casilla no contiene nada:
+                print(
+                    f"hizo click en el cuadrado de la columna {self.columna}, fila {self.fila}. indice: {self.indice}contiene {self.contiene}")
+                # si tienes algo en el mouse y la casilla no contiene nada:
                 if ahoritaTiene != [] and self.contiene == []:
-                    #cambiar las coordenadas
-                    ahoritaTiene[0].x = self.x + self.width//2
-                    ahoritaTiene[0].y = self.y + self.height//2
                     self.contiene.append(ahoritaTiene[0])
-                    ahoritaTiene.pop()
+                    ahoritaTiene.pop(0)
 
     def mostrarLoQueContiene(self):
-        if self.contiene != []:
+        if self.contiene:
+            #Cambiar coordenadas
+            self.contiene[0].x = self.x + self.width // 2
+            self.contiene[0].y = self.y + self.height // 2
             self.contiene[0].dibujarDefensa()
 
 
@@ -107,27 +173,26 @@ class RectanguloClaro:
         self.fila = fila
         self.indice = indice
         self.contiene = []
+
     def dibujar(self):
         pygame.draw.rect(win, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
 
     def detectarClick(self, mx, my):
         if (self.y + self.height) > my > self.y:
             if self.x < mx < (self.x + self.width):
-                print(f"hizo click en el cuadrado de la columna {self.columna}, fila {self.fila}. indice: {self.indice}, contiene {self.contiene}")
-                #si tienes algo en el mouse y la casilla no contiene nada:
+                print(
+                    f"hizo click en el cuadrado de la columna {self.columna}, fila {self.fila}. indice: {self.indice}, contiene {self.contiene}")
+                # si tienes algo en el mouse y la casilla no contiene nada:
                 if ahoritaTiene != [] and self.contiene == []:
-                    #cambiar las coordenadas
-                    ahoritaTiene[0].x = self.x + self.width//2
-                    ahoritaTiene[0].y = self.y + self.height//2
                     self.contiene.append(ahoritaTiene[0])
-                    ahoritaTiene.pop()
-        self.mostrarLoQueContiene()
+                    ahoritaTiene.pop(0)
 
     def mostrarLoQueContiene(self):
         if self.contiene != []:
+            #cambiar las coordenadas
+            self.contiene[0].x = self.x + self.width // 2
+            self.contiene[0].y = self.y + self.height // 2
             self.contiene[0].dibujarDefensa()
-    
-                    
 
 
 verdefosfo = (79, 222, 59)
@@ -139,7 +204,8 @@ Rectangulos = []
 
 CuadradosPlantas = []
 
-defensasEscogidas = []
+defensasEscogidas = [Piñata(-100, -100), Piñata2(-100, -100), Piñata3(-100, -100), Piñata4(-100, -100),
+                     Piñata5(-100, -100)]
 
 ahoritaTiene = []
 
@@ -187,10 +253,10 @@ while run:
                 fila += 1
             if columna >= 9:
                 columna = 0
+
     for rectangulo in Rectangulos:
         rectangulo.dibujar()
         rectangulo.mostrarLoQueContiene()
-
 
     # Dibujar cuadro plantas
     rectanguloPlantas.dibujarRectangulo()
@@ -202,9 +268,14 @@ while run:
 
     for cuadro in CuadradosPlantas:
         cuadro.dibujarCuadrado()
-        #Lo que contiene cada cuadrado
+        #si esta vacio entonces le metemos el objeto
+        if len(cuadro.contains) == 0:
+            for defensa in defensasEscogidas:
+                #se hace para que no quite de defensas escogidas
+                objDefensa = defensa
+                cuadro.contains.append(objDefensa)
+        # Lo que contiene cada cuadrado
         cuadro.mostrarLoQueContiene()
-
 
     # print(mx, my)
     pygame.time.delay(10)
