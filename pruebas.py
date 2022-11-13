361e4e63039a76b1bc53a9314ccb9d96d5f979b1
tartaman <victor.ealpuche@prepafrayangelico.edu.mx>
2022-11-13 13:20:47 -0600

    Enemigos?

--- pruebas.py
+++ pruebas.py
@@ -23,14 +23,67 @@ falta:
 -Mecanicas de cada planta
 """
 run = True
+Oleada = 1
+
+
+class BotonSig:
+    def __init__(self):
+        self.x = 1150
+        self.y = 0
+        self.width = 100
+        self.height = 100
+        self.color = (60, 179, 113)
+        self.clickeable = True
+    def dibujar(self):
+        pygame.draw.rect(win, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
+
+    def detectarClick(self, mx, my):
+        if (self.y + self.height) > my > self.y:
+            if self.x < mx < (self.x + self.width):
+                print("Hizo click")
+
+                if len(Enemigos) == 0 and self.clickeable:
+                    #si se puede clickear significa que acabo la oleada
+                    self.clickeable = False
+                    # todo 5 enemigos por oleada
+                    for i in range(Oleada * 5):
+                        Enemigos.append(Enemigo())
+                        # Lo que vamos a hacer es que cuando el enemigo muera, lo quitamos de la lista y pues muere, sabes
+                        # y cuando la lista ya no tenga, va a volver a poner enemigos.
+                print(f"en enemigos hay: {Enemigos}")
+
+
 
 class Enemigo:
-    def __init__(self,x , y, fila):
-        self.x = x
-        self.y = y
-        self.fila = fila
+    def __init__(self):
+        self.x = 1155
+        self.y = -100
+        self.velx = 3
+        self.salud = 100
+        self.fila = random.randint(0, 4)
+
     def dibujar(self):
-        pygame.draw.circle(win, (0, 0, 0),(self.x, self.y), 35)
+        pygame.draw.circle(win, (0, 0, 0), (self.x, self.y), 35)
+    def mover(self):
+        self.x -= self.velx
+    def ySegunSuFila(self):
+        """"como no hay switch pos ajam puro if"""
+        if fila == 0:
+            self.y = 150
+        if fila == 1:
+            self.y = 250
+        if fila == 2:
+            self.y = 350
+        if fila == 3:
+            self.y = 450
+        if fila == 4:
+            self.y = 550
+    def seMurio(self):
+        if self.salud > 0:
+            return False
+        else:
+            return True
+
 class CuadroPala:
     def __init__(self, x, y):
         self.x = x
@@ -38,6 +91,7 @@ class CuadroPala:
         self.width = 100
         self.height = 100
         self.color = (0, 0, 0)
+
     def dibujar(self):
         pygame.draw.rect(win, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
 
@@ -61,7 +115,8 @@ class Piñata:
 
     def dibujarDefensa(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
-    #en cuadrado
+
+    # en cuadrado
     def dibujarDefensaEC(self, x, y):
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
@@ -74,6 +129,7 @@ class Piñata2:
         # radio por ahora pq ajam
         self.radius = 35
         self.color = (255, 255, 0)
+
     def dibujarDefensa(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
@@ -89,6 +145,7 @@ class Piñata3:
         # radio por ahora pq ajam
         self.radius = 35
         self.color = (255, 0, 255)
+
     def dibujarDefensa(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
@@ -104,6 +161,7 @@ class Piñata4:
         # radio por ahora pq ajam
         self.radius = 35
         self.color = (255, 255, 255)
+
     def dibujarDefensa(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
@@ -119,6 +177,7 @@ class Piñata5:
         # radio por ahora pq ajam
         self.radius = 35
         self.color = (0, 255, 255)
+
     def dibujarDefensa(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
@@ -157,9 +216,9 @@ class CuadroPlantas:
         if (self.y + self.height) > my > self.y:
             if self.x < mx < (self.x + self.width):
                 print(f"Escogio el cuadrado {self.indice}, que contiene {self.contains}")
-                #todo si no tiene agarrado nada entonces agarra si no pues no vea
+                # todo si no tiene agarrado nada entonces agarra si no pues no vea
                 if len(ahoritaTiene) == 0:
-                    #todo Crea un objeto nuevo fuera de la pantalla para agregar
+                    # todo Crea un objeto nuevo fuera de la pantalla para agregar
                     defensa = self.contains[self.indice]
                     ahoritaTiene.append(defensa)
                     print(f"ahorita tiene: {ahoritaTiene}")
@@ -168,8 +227,8 @@ class CuadroPlantas:
                     print(f"Ahorita tiene: {ahoritaTiene}")
 
     def mostrarLoQueContiene(self):
-        self.contains[self.indice].x = self.x + self.width//2
-        self.contains[self.indice].y = self.y + self.height//2
+        self.contains[self.indice].x = self.x + self.width // 2
+        self.contains[self.indice].y = self.y + self.height // 2
         self.contains[self.indice].dibujarDefensa()
 
 
@@ -208,7 +267,7 @@ class RectanguloOscuro:
 
     def mostrarLoQueContiene(self):
         if self.contiene:
-            #Cambiar coordenadas
+            # Cambiar coordenadas
             self.contiene[0].x = self.x + self.width // 2
             self.contiene[0].y = self.y + self.height // 2
             self.contiene[0].dibujarDefensa()
@@ -249,7 +308,7 @@ class RectanguloClaro:
 
     def mostrarLoQueContiene(self):
         if self.contiene != []:
-            #cambiar las coordenadas
+            # cambiar las coordenadas
             self.contiene[0].x = self.x + self.width // 2
             self.contiene[0].y = self.y + self.height // 2
             self.contiene[0].dibujarDefensa()
@@ -266,11 +325,14 @@ CuadradosPlantas = []
 
 defensasEscogidas = [Piñata(-100, -100), Piñata2(-100, -100), Piñata3(-100, -100), Piñata4(-100, -100),
                      Piñata5(-100, -100)]
+# todo crear enemigos
+Enemigos = []
 
 ahoritaTiene = []
 
 rectanguloPlantas = RectanguloPlantas(((width - (width // 3)) // 2), 0)
-cuadroPala = CuadroPala(0,0)
+cuadroPala = CuadroPala(0, 0)
+botonSig = BotonSig()
 while run:
     win.fill((255, 255, 255))
     mx, my = pygame.mouse.get_pos()
@@ -278,13 +340,14 @@ while run:
     for event in ev:
         if event.type == pygame.QUIT:
             run = False
-        #todo aqui abajo se pone lo que necesite ser con click
+        # todo aqui abajo se pone lo que necesite ser con click
         if event.type == pygame.MOUSEBUTTONUP:
             for rectangulo in Rectangulos:
                 rectangulo.detectarClick(mx, my)
             for cuadrado in CuadradosPlantas:
                 cuadrado.detectarClick(mx, my)
             cuadroPala.detectarClick(mx, my)
+            botonSig.detectarClick(mx, my)
 
     # Crear cuadricula
     if len(Rectangulos) == 0:
@@ -331,17 +394,25 @@ while run:
 
     for cuadro in CuadradosPlantas:
         cuadro.dibujarCuadrado()
-        #si esta vacio entonces le metemos el objeto
+        # si esta vacio entonces le metemos el objeto
         if len(cuadro.contains) == 0:
             for defensa in defensasEscogidas:
-                #se hace para que no quite de defensas escogidas
+                # se hace para que no quite de defensas escogidas
                 objDefensa = defensa
                 cuadro.contains.append(objDefensa)
         # Lo que contiene cada cuadrado
         cuadro.mostrarLoQueContiene()
-    #Dibujar pala
+    # Dibujar pala
     cuadroPala.dibujar()
+    #Dibujar boton sig
+    botonSig.dibujar()
+    #Dibujar enemigos si hay enemigos
+    
+    for enemigo in Enemigos:
+        enemigo.ySegunSuFila()
+        enemigo.dibujar()
+        enemigo.mover()
 
-    # print(mx, my)
+    #print(mx, my)
     pygame.time.delay(10)
     pygame.display.update()
